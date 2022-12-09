77053b80181486442a67d96c7d32ac56476bf09c
Ufosky58@gmail.com <Ufosky58@gmail.com>
2022-12-08 17:59:47 -0800

    Printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	/* This function prints out text using the printf function */
+
+	char my_char[] = "with proper grammar, but the outcome is a piece of art,\n";
+
+	printf("%s", my_char);
+	return (0);
+}
