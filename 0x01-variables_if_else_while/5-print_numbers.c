6e28f290b11da498855eaa82a422ef6d7726a1a8
Ufosky58@gmail.com <Ufosky58@gmail.com>
2022-12-09 14:43:32 -0800

    Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	/* Program to print single digits from 0 - 9 using printf */
+
+	int nums;
+
+	for (nums = 0; nums < 10; nums++)
+		printf("%d", nums);
+	printf("\n");
+	return (0);
+}
