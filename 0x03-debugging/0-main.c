7320b8670bed1ed56dc239f7848f6116c6766938
Ufosky58@gmail.com <Ufosky58@gmail.com>
2022-12-15 06:25:00 -0800

    Multiples main

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -4,12 +4,13 @@
  * main - Test function that prints if integer is positive or negative
  * Return: 0
  */
+
 int main(void)
 {
-	int a;
+	int i;
 
-	a = 0;
-	positive_or_negative(a);
+	i = 0;
+	positive_or_negative(i);
 
 	return (0);
 }
