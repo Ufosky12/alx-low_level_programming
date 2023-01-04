4eaf32da5b1d4aeb343bc8ee58f82ac6561c9e49
Ufosky58@gmail.com <Ufosky58@gmail.com>
2023-01-04 06:53:06 -0800

    Square root

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _sqrt_recursion - returns the natural square root of a number
+ * @n: base number
+ * Return: the square root of n
+ */
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+		return (-1);
+	if (n == 0 || n == 1)
+		return (n);
+}
