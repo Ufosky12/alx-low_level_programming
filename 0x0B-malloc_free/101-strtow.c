b92ab1268b8a75e64fa28a33338db3c86526a9d1
Ufosky58@gmail.com <Ufosky58@gmail.com>
2023-01-11 09:00:46 -0800

    splint strings

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -1,10 +1,9 @@
-include < stdlib.h >
+#include <stdlib.h>
 #include "main.h"
 
 /**
  * count_word - helper function to count the number of words in a string
  * @s: string to evaluate
- *
  * Return: number of words
  */
 
